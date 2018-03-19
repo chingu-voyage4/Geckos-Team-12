43a8e8600706c56ff71f4519d0f138366c9583e0
yajouahr <yassine.jouahri@livion.fi>
2018-03-19 19:35:26 +0200

    NavBar component with basic css and basic auth logic

--- client/src/containers/Navbar.js
+++ client/src/containers/Navbar.js
@@ -1,23 +1,72 @@
 import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+
+import { signInUser, signOutUser } from "../redux/Auth";
+import "../assets/css/navBar.css";
 
 export class Navbar extends Component {
-  goHome = () => this.props.history.push("/");
-  goCatalog = () => this.props.history.push("/catalog");
-  goUser = () => this.props.history.push("/user");
+  signOut = () => this.props.signOut;
+  handleSignIn = () => {
+    this.props.signIn();
+    this.props.history.push("/mygarden");
+  };
+  handleSignOut = () => {
+    this.props.signOut();
+    this.props.history.push("/");
+  };
   render() {
+    const { auth } = this.props;
     return (
       <div className="nav-bar">
-        <div>
-          <button onClick={this.goHome}>Home</button>
-          <button onClick={this.goCatalog}>Catalog</button>
+        <div
+          className="nav-bar-brand"
+          onClick={() => this.props.history.push("/")}
+        >
+          SimpleSeed
         </div>
-        <div>
-          <button onClick={this.goUser}>User</button>
+        <div className="nav-bar-menu">
+          <Link className="" to="/Catalog">
+            Catalog
+          </Link>
+          <Link className="" to="/mygarden">
+            MyGarden
+          </Link>
+        </div>
+        <div className="nav-bar-user">
+          {!auth && (
+            <div className="signin">
+              <button onClick={this.handleSignIn}>Sign In</button>
+              {/* <Link to="/signin">SignIn</Link> */}
+              <Link to="/signup">SignUp</Link>
+            </div>
+          )}
+          {auth && (
+            <div className="user">
+              <button className="signout" onClick={this.handleSignOut}>
+                Sign Out
+              </button>
+              <button
+                className="profile"
+                to="/profile"
+                onClick={() => this.props.history.push("/profile")}
+              />
+            </div>
+          )}
         </div>
       </div>
     );
   }
 }
+const mapStateToProps = state => {
+  return {
+    auth: state.auth.authenticated
+  };
+};
+const mapDispatchToProps = dispatch => ({
+  signIn: () => dispatch(signInUser()),
+  signUp: () => dispatch(signInUser()),
+  signOut: () => dispatch(signOutUser())
+});
 
-export default withRouter(Navbar);
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Navbar));
