0bde87aea85fe73491fc10c6879ab23b59910bef
hixsav <hi.savis@gmail.com>
2018-03-22 09:40:55 +0000

    Added more fields to user.js in models directory

--- server/models/user.js
+++ server/models/user.js
@@ -4,12 +4,22 @@ const Schema = mongoose.Schema;
 // user schema definition
 const UserSchema = new Schema(
   {
-    user_name: {type: String, required: true, max: 100},
-    family_name: {type: String, required: true, max: 100},
-    date_of_birth: {type: Date},
-    email: {type: String, lowercase: true, required: true, match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
-    profession: {type: String, required: true, max: 100},
-  }, {timestamps: true});
+    username: {
+      type: String, 
+      required: true, 
+    },
+    password: {
+      type: String, 
+      required: true, 
+      max: 20
+    },
+    growingCrops: {
+      type: String
+    },
+    score: {
+      type: String
+    }, 
+    {timestamps: true});
 
 // Sets the timestamp createdAt parameter equal to the current time
 CropSchema.pre('save', next => {
