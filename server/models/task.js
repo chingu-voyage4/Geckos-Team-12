3c6cd1377e36c958692287d7150c0f0c712764a4
yajouahr <yassine.jouahri@livion.fi>
2018-03-25 22:36:11 +0300

    task model creation

---
+++ server/models/task.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// crop schema definition
+const Task = new Schema({
+  name: {
+    type: String,
+    required: "Please enter a crop name"
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  short_desc: {
+    type: String
+  },
+  difficulty_level: {
+    type: String
+  },
+  steps: [{ title: String, description: String, status: Boolean }],
+  crop_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Crop"
+  },
+  createdAt: Date
+});
+
+// Export crop schema model for use elsewhere
+module.exports = mongoose.model("Task", Task);
